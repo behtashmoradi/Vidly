886acd1c3e634f0bcf3a9fd1768ddb6e3db93cb9
Behtash Moradi <behtash.moradi@live.com>
2020-01-12 10:07:54 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Movies from "./Components/Movies";
+
+function App() {
+  return (
+    <div className="container">
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
